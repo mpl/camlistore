041d4657c3fb0393baa55237c7ffd8db2d52794b
mpl <mathieu.lonjaret@gmail.com>
2014-09-20 00:59:37 +0200

    cleanup for 8)
    Change-Id: I74ced39c005444987c3d85f3f629315783443816

--- pkg/importer/scanning_cabinet/scancab.go
+++ pkg/importer/scanning_cabinet/scancab.go
@@ -17,24 +17,17 @@ limitations under the License.
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
-	"strings"
 	"time"
-
-	"code.google.com/p/goauth2/oauth"
 
 	"github.com/golang/oauth2"
 	"github.com/golang/oauth2/google"
 	gcdatastore "google.golang.org/cloud/datastore"
-	gcstorage "google.golang.org/cloud/storage"
-	gcstorage2 "github.com/GoogleCloudPlatform/gcloud-golang/storage"
-
-	oauth1 "camlistore.org/third_party/github.com/garyburd/go-oauth/oauth"
+//	gcstorage "google.golang.org/cloud/storage"
+//	gcstorage2 "github.com/GoogleCloudPlatform/gcloud-golang/storage"
 )
 
 /*
@@ -46,6 +39,10 @@ as the blobs don't seem to be in the bucket when I ls with gsutil.
 4) oauth with github.com/garyburd/go-oauth/oauth -> getting a 400, but maybe I half-assed it.
 5) went back to github.com/golang/oauth2, and added X-AppEngine-User-Email header -> not better.
 6) go doc hinted at the problem: there's still a login: required in app.yaml, that oauth does not override. need to test and confirm (that we're ok without it).
+7) back to approach in 1): was getting 403 because GCS JSON API was needed too. Getting 404s now.
+But looked through API explorer at https://developers.google.com/apis-explorer/#p/storage/v1/storage.objects.list
+which shows same as with gsutil, i.e. not my files. So probably no go that way.
+8) back to 6).
 */
 
 var (
@@ -54,13 +51,6 @@ var (
 	myEmail = "mathieu.lonjaret@gmail.com"
 	ds *gcdatastore.Dataset
 	cl *http.Client
-	clientId = "886924983567-hnd1dertfvi2g0lpjs72aae8hi35k364.apps.googleusercontent.com"
-	clientSecret = "XYta4alhMIjTHK5mreBk-fXU"
-	oa1tempCreds *oauth1.Credentials
-	oa1Creds *oauth1.Credentials
-	oa1CallbackURL = "http://home.granivo.re:8009/callback"
-	host = ":8080"
-	gotCreds chan struct{}
 )
 
 // UserInfo represents the metadata associated with the Google User
@@ -216,156 +206,6 @@ func getScannedFile(key string) error {
 	return ioutil.WriteFile("/home/mpl/glenda.png", body, 0700)
 }
 
-var ()
-
-func transportFromAPIKey() (*oauth.Transport, error) {
-        var config = &oauth.Config{
-//				Scope: "https://www.googleapis.com/auth/userinfo.email",
-				Scope: "https://www.googleapis.com/auth/appengine",
-                ClientId:     clientId,
-                ClientSecret: clientSecret,
-                AuthURL:      "https://accounts.google.com/o/oauth2/auth",
-                TokenURL:     "https://accounts.google.com/o/oauth2/token",
-                RedirectURL:  "urn:ietf:wg:oauth:2.0:oob",
-                TokenCache:   oauth.CacheFile("/home/mpl/tokencache.json"),
-        }
-        // Set up a Transport using the config.
-        transport := &oauth.Transport{Config: config}
-
-        // Try to pull the token from the cache; if this fails, we need to get one.
-        token, err := config.TokenCache.Token()
-        if err != nil {
-                // Get an authorization code from the data provider.
-                // ("Please ask the user if I can access this resource.")
-                fmt.Printf("Visit this URL to get a code: %v\n", config.AuthCodeURL(""))
-                input := bufio.NewReader(os.Stdin)
-                line, _, err := input.ReadLine()
-                if err != nil {
-                        log.Fatalf("Failed to read line: %v", err)
-                }
-                code := strings.TrimSpace(string(line))
-
-                // Exchange the authorization code for an access token.
-                // ("Here's the code you gave the user, now give me a token!")
-                token, err = transport.Exchange(code)
-                if err != nil {
-                        log.Fatalf("Exchange: %v", err)
-                }
-        }
-        // Make the actual request using the cached token to authenticate.
-        // ("Here's the token, let me in!")
-        transport.Token = token
-        return transport, nil
-}
-
-func transportFromAPIKey2() (*oauth2.Transport, error) {
-	conf, err := oauth2.NewConfig(&oauth2.Options{
-				Scopes: []string{"https://www.googleapis.com/auth/appengine.admin",
-					"https://www.googleapis.com/auth/userinfo.email"},
-                ClientID:     clientId,
-                ClientSecret: clientSecret,
-                RedirectURL:  "urn:ietf:wg:oauth:2.0:oob",
-		},
-		"https://accounts.google.com/o/oauth2/auth",
-		"https://accounts.google.com/o/oauth2/token")
-	if err != nil {
-		return nil, err
-	}
-
-	// Redirect user to consent page to ask for permission
-	// for the scopes specified above.
-	url := conf.AuthCodeURL("state", "online", "auto")
-//	url := conf.AuthCodeURL("state", "offline", "auto")
-	fmt.Printf("Visit the URL for the auth dialog: %v\n", url)
-
-	input := bufio.NewReader(os.Stdin)
-	line, _, err := input.ReadLine()
-	if err != nil {
-		log.Fatalf("Failed to read line: %v", err)
-	}
-	authorizationCode := strings.TrimSpace(string(line))
-
-	return conf.NewTransportWithCode(authorizationCode)
-}
-
-func oauth1Client() *oauth1.Client {
-	return &oauth1.Client{
-		TemporaryCredentialRequestURI: "https://scancabcamli.appspot.com/_ah/OAuthGetRequestToken",
-		ResourceOwnerAuthorizationURI: "https://scancabcamli.appspot.com/_ah/OAuthAuthorizeToken",
-		TokenRequestURI:               "https://scancabcamli.appspot.com/_ah/OAuthGetAccessToken",
-		Credentials: oauth1.Credentials{
-			Token:  clientId,
-			Secret: clientSecret,
-		},
-	}
-}
-
-func tempCreds() {
-	oauthClient := oauth1Client()
-	tempCred, err := oauthClient.RequestTemporaryCredentials(&http.Client{}, oa1CallbackURL, nil)
-	if err != nil {
-		log.Fatalf("Error getting temp cred: %v", err)
-	}
-	oa1tempCreds = tempCred
-}
-
-func serveOAuthCallback(w http.ResponseWriter, r *http.Request) {
-	oauthClient := oauth1Client()
-	if oa1tempCreds.Token != r.FormValue("oauth_token") {
-		log.Fatalf("unexpected oauth_token: got %v, want %v", r.FormValue("oauth_token"), oa1tempCreds.Token)
-	}
-	tokenCred, _, err := oauthClient.RequestToken(
-		&http.Client{},
-		&oauth1.Credentials{
-			Token:  oa1tempCreds.Token,
-			Secret: oa1tempCreds.Secret,
-		},
-		r.FormValue("oauth_verifier"),
-	)
-	if err != nil {
-		log.Fatalf("Error getting final token: %v", err)
-	}
-	oa1Creds = tokenCred
-	fmt.Fprintf(w, "whatever")
-	gotCreds <- struct{}{}
-}
-
-func doOAuth1() {
-	http.HandleFunc("/callback", serveOAuthCallback)
-	go func() {
-		log.Fatal(http.ListenAndServe(host, nil))
-	}()
-	gotCreds = make(chan struct{})
-	tempCreds()
-	<- gotCreds
-	oauthClient := oauth1Client()
-	resp, err := oauthClient.Get(&http.Client{},
-		oa1Creds,
-		"https://scancabcamli.appspot.com/resource/5066549580791808/glenda.png",
-		nil,
-	)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode != http.StatusOK {
-		log.Fatalf("Get request on failed with: %s", resp.Status)
-	}		
-	if resp.StatusCode != 200 {
-		log.Fatalf("Status %v", resp.Status)
-	}
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	err = ioutil.WriteFile("/home/mpl/glenda.png", body, 0700)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return
-}
-
 func main() {
 	pemKeyBytes, err := ioutil.ReadFile("/home/mpl/scancabcamli-496f5f6eb01b.pem")
 	if err != nil {
@@ -375,53 +215,19 @@ func main() {
 		Email:      serviceAccount,
 		PrivateKey: pemKeyBytes,
 		Scopes:     []string{
-			gcstorage2.ScopeFullControl,
+//			gcstorage2.ScopeFullControl,
+			"https://www.googleapis.com/auth/appengine.admin",
+			"https://www.googleapis.com/auth/userinfo.email",
 		},
 	})
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	bkt := gcstorage2.New(conf.NewTransport()).Bucket(projectId)
-	_, err = bkt.Stat("glenda.png")
-	if err != nil {
-		log.Fatalf("stat: %v", err)
-	}
-	return
-
-	tr, err := transportFromAPIKey2()
-	if err != nil {
-		log.Fatal(err)
-	}
-	cl = &http.Client{Transport: tr}
+	cl = &http.Client{Transport: conf.NewTransport()}
 	scanBlobKey := "5066549580791808"
 	if err := getScannedFile(scanBlobKey); err != nil {
 		log.Fatal(err)
-	}
-	return
-
-	cl = &http.Client{Transport: conf.NewTransport()}
-//	scanBlobKey := "5066549580791808"
-	if err := getScannedFile(scanBlobKey); err != nil {
-		log.Fatal(err)
-	}
-
-	println("STORAGE")
-	bk, err := gcstorage.NewBucket(projectId, serviceAccount, pemKeyBytes)
-	if err != nil {
-		log.Fatal(err)
-	}
-	_, err = bk.Stat("datastore_backup_datastore_backup_2014_09_10_UserInfo-1576947681876AC4320CD-output-0-attempt-1")
-	if err != nil {
-		log.Fatalf("stat: %v", err)
-	}
-
-	objs, _, err := bk.List(nil)
-	if err != nil {
-		log.Fatalf("list: %v", err)
-	}
-	for _,v := range objs {
-		fmt.Printf("%T\n", v)
 	}
 	return
 
@@ -451,74 +257,3 @@ func main() {
 		}
 	}
 }
-
-/*
-
-//	"code.google.com/p/goauth2/oauth/jwt"
-//	"code.google.com/p/google-api-go-client/datastore/v1beta2"
-
-func withAutoGoogAPI() {
-	svc, err := datastore.New(transport.Client())
-	if err != nil {
-		log.Fatalf("New service: %v", err)
-	}
-	dsvc := datastore.NewDatasetsService(svc)
-
-	rqr := datastore.RunQueryRequest{
-		Query: &datastore.Query{
-			Kinds: []*datastore.KindExpression{
-				{
-					Name: "UserInfo",
-				},
-			},
-			Filter: &datastore.Filter{
-				PropertyFilter: &datastore.PropertyFilter{
-					Operator: "equal",
-					Property: &datastore.PropertyReference{"User"},
-					Value: &datastore.Value{
-						StringValue: email,
-					},
-				},
-			},
-		},
-	}
-
-	// RunQuery: Query for entities.
-	call := dsvc.RunQuery("scancabcamli", &rqr)
-
-	lookup := datastore.LookupRequest{
-		Keys: []*datastore.Key{
-			{
-			Path: []*datastore.KeyPathElement{
-				{
-					Kind: "Document",
-					Id: 5144752345317376,
-				},
-			},
-			},
-		},
-	}
-	call := dsvc.Lookup("scancabcamli", &lookup)
-	_, err = call.Do()
-	if err != nil {
-		log.Fatalf("query: %v", err)
-	}
-}
-
-func withJWT() {
-	// Craft the ClaimSet and JWT token.
-	iss := "886924983567-uiln6pus9iuumdq3i0vav0ntveodas0r@developer.gserviceaccount.com"
-	//scope := "https://www.googleapis.com/auth/devstorage.read_only"
-	scope := datastore.DatastoreScope
-	pemKeyBytes, err := ioutil.ReadFile("/home/mpl/scancabcamli-496f5f6eb01b.pem")
-	if err != nil {
-		log.Fatal(err)
-	}
-//	tr, err := jwt.NewTransport(jwt.NewToken(iss, scope, pemKeyBytes))
-	_, err = jwt.NewTransport(jwt.NewToken(iss, scope, pemKeyBytes))
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-*/
